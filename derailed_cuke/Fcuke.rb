823f7b89f3386708ee281f979d3790bf0654e616
abhishekkr <abhishk@thoughtworks.com>
2011-08-17 12:41:09 +0530

    now all cuke test logs get stored in derailed_cuke/log/ directory sepearately as feature_name.log

--- derailed_cuke/Fcuke.rb
+++ derailed_cuke/Fcuke.rb
@@ -1,16 +1,19 @@
-SHEL_LOG="twrak.log"
+LOGS_DIR="./log"
 RUBY_BIN="ruby"
 
 def run_cuke()
-    clean_log =  %x{echo '' > #{SHEL_LOG}}
+    clean_log =  %x{ rm -f #{(LOGS_DIR+"/*").to_s} }
     bi = %x{#{RUBY_BIN} -S bundle install}
     files = Dir.glob("features/*.feature")
     files.each do |feat|
         print "*"*100 + "\nTesting Feature: " + feat.to_s + "\n" + "_"*100 + "\n"
 	p "#{RUBY_BIN} -S rake features FEATURE=#{feat.to_s}  --trace"
-        rake_feature = %x{#{RUBY_BIN} -S rake features FEATURE=#{feat.to_s}  --trace >> #{SHEL_LOG}}
-        echo_pgbrk = %x{echo '' >> #{SHEL_LOG}}
+        p %{echo ' #{RUBY_BIN} -S rake features FEATURE=#{feat.to_s}  --trace >> #{LOGS_DIR+"/"+feat.to_s+"log"}' }
+        log_file = LOGS_DIR + "/" + feat.split('/')[1].split('.')[0] + ".log"
+        rake_feature = %x{ #{RUBY_BIN} -S rake features FEATURE=#{feat.to_s}  --trace >> #{log_file} }
+        echo_pgbrk = %x{echo '' >> #{log_file}}
     end
+    p "cuke logs can be viewed at ./log dir"
 end
 
 run_cuke
